e77ee0b5ab06e0f5a5fe4056d1111512991ccc05
alancsoliveira <alancristian@alu.ufc.br>
2023-08-21 11:12:19 -0300

    Github Favorites

---
+++ Projeto16 - github-favorites/js/main.js
@@ -0,0 +1,29 @@
+import { FavoritesView } from "./Favorites.js";
+
+new FavoritesView('#app')
+
+
+
+
+/*
+[
+      {
+        login: "alancsoliveira",
+        name: "Alan Cristian",
+        public_repos: "75",
+        followers: "1",
+      },
+      {
+        login: "jamezaguiar",
+        name: "Jamerson Aguiar",
+        public_repos: "76",
+        followers: "2",
+      },
+      {
+        login: "rafaelviana",
+        name: "Rafael Viana",
+        public_repos: "77",
+        followers: "3",
+      },
+    ]
+    */
